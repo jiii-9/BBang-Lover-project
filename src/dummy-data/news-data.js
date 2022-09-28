93c63ff45abd25ea82712936360cfa378c7be11f
jiii-9 <succeed.g9@gmail.com>
2022-09-28 17:58:41 +0900

    dummy-data

---
+++ src/dummy-data/news-data.js
@@ -0,0 +1,70 @@
+const newsData = [
+  {
+    id: 0,
+    title: "빵순이들 사이에서 소문난, 전국 소금빵 맛집 5곳",
+    url: "https://www.siksinhot.com/theme/magazine/2580",
+    image: "/assets/news-data-0.jpg",
+  },
+  {
+    id: 1,
+    title: "빵순이가 뽑은! 울산 빵집 6곳",
+    url: "https://www.siksinhot.com/theme/magazine/1941",
+    image: "/assets/news-data-1.jpg",
+  },
+  {
+    id: 2,
+    title: "서울 샌드위치 맛집 BEST 5",
+    url: "https://www.siksinhot.com/theme/magazine/2902",
+    image: "/assets/news-data-2.jpg",
+  },
+  {
+    id: 3,
+    title: "지역별 유명 디저트",
+    url: "https://www.siksinhot.com/theme/magazine/2824",
+    image: "/assets/news-data-3.jpg",
+  },
+  {
+    id: 4,
+    title: "달콤하게 녹아내리는, 수제 케이크 맛집 BEST 5",
+    url: "https://www.siksinhot.com/theme/magazine/2787",
+    image: "/assets/news-data-4.jpg",
+  },
+  {
+    id: 5,
+    title: "부드럽고 달콤한 매력! 팬케이크 맛집 BEST 5",
+    url: "https://www.siksinhot.com/theme/magazine/2723",
+    image: "/assets/news-data-5.jpg",
+  },
+  {
+    id: 6,
+    title: "휴식같은 올데이 브런치",
+    url: "https://www.siksinhot.com/theme/magazine/2859",
+    image: "/assets/news-data-6.jpg",
+  },
+  {
+    id: 7,
+    title: "동서양의 대통합! 전국 생크림 팥빵 맛집 5곳",
+    url: "https://www.siksinhot.com/theme/magazine/2683",
+    image: "/assets/news-data-7.jpg",
+  },
+  {
+    id: 8,
+    title: "이 구역의 빵집은 바로 나! 전국 로컬 빵집 5곳",
+    url: "https://www.siksinhot.com/theme/magazine/2680",
+    image: "/assets/news-data-8.jpg",
+  },
+  {
+    id: 9,
+    title: "입안 가득 맴도는 달콤함! 도넛 맛집 BEST 5",
+    url: "https://www.siksinhot.com/theme/magazine/2667",
+    image: "/assets/news-data-9.jpg",
+  },
+  {
+    id: 10,
+    title: "극강의 달콤한! 초콜릿 디저트 맛집 5곳",
+    url: "https://www.siksinhot.com/theme/magazine/2660",
+    image: "/assets/news-data-10.jpg",
+  },
+];
+
+export default newsData;
---
+++ src/dummy-data/select-data.js
@@ -0,0 +1,79 @@
+const characterData = [
+  {
+    id: 0,
+    name: "#달콤한",
+  },
+  {
+    id: 1,
+    name: "#느끼한",
+  },
+  {
+    id: 2,
+    name: "#고소한",
+  },
+  {
+    id: 3,
+    name: "#건강한",
+  },
+  {
+    id: 4,
+    name: "#짭짤한",
+  },
+  {
+    id: 5,
+    name: "#부드러운",
+  },
+  {
+    id: 6,
+    name: "#담백한",
+  },
+  {
+    id: 7,
+    name: "#쫄깃한",
+  },
+  {
+    id: 8,
+    name: "#바삭한",
+  },
+  {
+    id: 9,
+    name: "#촉촉한",
+  },
+];
+
+const areaData = [
+  {
+    id: 0,
+    name: "#서울",
+  },
+  {
+    id: 1,
+    name: "#경기",
+  },
+  {
+    id: 2,
+    name: "#인천",
+  },
+  {
+    id: 3,
+    name: "#강원도",
+  },
+  {
+    id: 4,
+    name: "#충청도",
+  },
+  {
+    id: 5,
+    name: "#전라도",
+  },
+  {
+    id: 6,
+    name: "#경상도",
+  },
+  {
+    id: 7,
+    name: "#제주도",
+  },
+];
+
+export default { characterData, areaData };
