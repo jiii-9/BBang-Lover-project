480c19b4a5d33b9bc91b5f6c33fc303171710ae0
jiii-9 <succeed.g9@gmail.com>
2022-09-29 23:18:26 +0900

    App.js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Start from "./pages/Start";
+import Home from "./pages/Home";
+import Recommend from "./pages/Recommend";
+import Detail from "./pages/Detail";
+import Footer from "./components/Footer";
+
+import characterData from "./dummy-data/select-data";
+import areaData from "./dummy-data/select-data";
+import newsData from "./dummy-data/news-data";
+
+import "./style/main.scss";
+
+export const SelectListContext = React.createContext();
+export const NewsListContext = React.createContext();
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SelectListContext.Provider value={(characterData, areaData)}>
+      <NewsListContext.Provider value={newsData}>
+        <BrowserRouter>
+          <div className="App">
+            <Routes>
+              <Route path="/start" element={<Start />} />
+              <Route path="/" element={<Home />} />
+              <Route path="/recommend" element={<Recommend />} />
+              <Route path="/detail/:id" element={<Detail />} />
+            </Routes>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </NewsListContext.Provider>
+    </SelectListContext.Provider>
   );
 }
 
