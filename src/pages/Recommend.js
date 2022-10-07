7209137ba050e616adab28d4cbe13309d9382771
jiii-9 <succeed.g9@gmail.com>
2022-10-08 01:03:12 +0900

    add selected button component

--- src/pages/Recommend.js
+++ src/pages/Recommend.js
@@ -1,18 +1,24 @@
+import React from "react";
 import { useLocation } from "react-router-dom";
 import Header from "../components/Header";
+import SelectedButtons from "../components/SelectedButtons";
+// import SelectedButtons from "../components/SelectedButtons";
 import Store from "../components/Store";
 
 function Recommend() {
   const location = useLocation();
   const checkedCharacterList = location.state.checkedCharacterList;
   const checkedAreaList = location.state.checkedAreaList;
-  console.log(checkedCharacterList, checkedAreaList);
 
   return (
-    <div>
+    <section className="recommend">
       <Header />
-      <Store />
-    </div>
+      <SelectedButtons
+        characterList={checkedCharacterList}
+        areaList={checkedAreaList}
+      />
+      <Store characterList={checkedCharacterList} areaList={checkedAreaList} />
+    </section>
   );
 }
 
