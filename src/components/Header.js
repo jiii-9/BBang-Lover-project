dc70ca3674fd6319e9f25745a670dd12734ccfca
jiii-9 <succeed.g9@gmail.com>
2022-10-10 10:42:39 +0900

    some markup modifications to be responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,38 @@
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBell, faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faBell } from "@fortawesome/free-solid-svg-icons";
 
+import Notify from "./Notify";
 import Logo from "../logo.svg";
 
 function Header() {
   const navigate = useNavigate();
-  const [search, setSearch] = useState("");
 
   const goHome = () => {
     navigate("/", { replace: true }); // Home으로 이동하고 뒤로가기는 되지 않음
   };
 
   return (
-    <header className="header">
-      <div className="inner">
-        <div className="header-box">
-          <div className="logo">
-            <img alt="logo" src={Logo} onClick={goHome} />
-          </div>
+    <>
+      <header className="header">
+        <div className="inner">
+          <div className="header-box">
+            <div className="logo">
+              <img alt="logo" src={Logo} onClick={goHome} />
+            </div>
 
-          <div className="search">
-            <FontAwesomeIcon
-              className="search__icon"
-              icon={faMagnifyingGlass}
-            />
-            <input
-              placeholder="궁금한 빵집을 검색하세요"
-              type="text"
-              value={search}
-              onChange={e => {
-                setSearch(e.target.value);
-              }}
-            />
-          </div>
+            <span className="logo__text" onClick={goHome}>
+              빵방곡곡
+            </span>
 
-          <div className="notification">
-            <FontAwesomeIcon className="notification__icon" icon={faBell} />
+            <div className="notification">
+              <FontAwesomeIcon className="notification__icon" icon={faBell} />
+            </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 }
 
