52d421baf4540667246c3863427d44d731185124
jiii-9 <succeed.g9@gmail.com>
2022-10-12 12:49:04 +0900

    news headline wrap

--- src/assets/news-data.js
+++ src/assets/news-data.js
@@ -1,13 +1,13 @@
 const newsData = [
   {
     id: 0,
-    title: "빵순이들 사이에서 소문난, 전국 소금빵 맛집 5곳",
+    title: "빵순이들 사이에서 소문난,\n전국 소금빵 맛집 5곳",
     url: "https://www.siksinhot.com/theme/magazine/2580",
     image: "/assets/news-data-0.jpg",
   },
   {
     id: 1,
-    title: "빵순이가 뽑은! 울산 빵집 6곳",
+    title: "빵순이가 뽑은!\n울산 빵집 6곳",
     url: "https://www.siksinhot.com/theme/magazine/1941",
     image: "/assets/news-data-1.jpg",
   },
@@ -25,13 +25,13 @@ const newsData = [
   },
   {
     id: 4,
-    title: "달콤하게 녹아내리는, 수제 케이크 맛집 BEST 5",
+    title: "달콤하게 녹아내리는,\n수제 케이크 맛집 BEST 5",
     url: "https://www.siksinhot.com/theme/magazine/2787",
     image: "/assets/news-data-4.jpg",
   },
   {
     id: 5,
-    title: "부드럽고 달콤한 매력! 팬케이크 맛집 BEST 5",
+    title: "부드럽고 달콤한 매력!\n팬케이크 맛집 BEST 5",
     url: "https://www.siksinhot.com/theme/magazine/2723",
     image: "/assets/news-data-5.jpg",
   },
@@ -43,25 +43,25 @@ const newsData = [
   },
   {
     id: 7,
-    title: "동서양의 대통합! 전국 생크림 팥빵 맛집 5곳",
+    title: "동서양의 대통합!\n전국 생크림 팥빵 맛집 5곳",
     url: "https://www.siksinhot.com/theme/magazine/2683",
     image: "/assets/news-data-7.jpg",
   },
   {
     id: 8,
-    title: "이 구역의 빵집은 바로 나! 전국 로컬 빵집 5곳",
+    title: "이 구역의 빵집은 바로 나!\n전국 로컬 빵집 5곳",
     url: "https://www.siksinhot.com/theme/magazine/2680",
     image: "/assets/news-data-8.jpg",
   },
   {
     id: 9,
-    title: "입안 가득 맴도는 달콤함! 도넛 맛집 BEST 5",
+    title: "입안 가득 맴도는 달콤함!\n도넛 맛집 BEST 5",
     url: "https://www.siksinhot.com/theme/magazine/2667",
     image: "/assets/news-data-9.jpg",
   },
   {
     id: 10,
-    title: "극강의 달콤한! 초콜릿 디저트 맛집 5곳",
+    title: "극강의 달콤한!\n초콜릿 디저트 맛집 5곳",
     url: "https://www.siksinhot.com/theme/magazine/2660",
     image: "/assets/news-data-10.jpg",
   },
