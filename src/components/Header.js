142b14a90416d69ed5df659fa77a7bd2f9027eee
jiii-9 <succeed.g9@gmail.com>
2022-11-20 21:09:34 +0900

    add condition to popup notification window

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,22 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+
+import Notification from "./Notification";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBell } from "@fortawesome/free-solid-svg-icons";
-
 import Logo from "../logo.svg";
 
 function Header() {
+  const [isClicked, setIsClicked] = useState(false);
   const navigate = useNavigate();
 
   const goHome = () => {
     navigate("/", { replace: true }); // Home으로 이동하고 뒤로가기는 되지 않음
+  };
+
+  const bellClickHandler = () => {
+    setIsClicked(isClicked => !isClicked);
   };
 
   return (
@@ -25,11 +32,12 @@ function Header() {
               빵방곡곡
             </span>
 
-            <div className="notification">
-              <FontAwesomeIcon className="notification__icon" icon={faBell} />
+            <div className="bell" onClick={bellClickHandler}>
+              <FontAwesomeIcon className="bell-icon" icon={faBell} />
             </div>
           </div>
         </div>
+        {isClicked ? <Notification isClicked={isClicked} /> : null}
       </header>
     </>
   );
